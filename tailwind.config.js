faaa4b43eb6fcaee5a54754277f70b3d2dd456df
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-17 23:40:58 +0200

    Add mobile screen styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        base: ['Raleway', 'sans-serif'],
+        body: ['Work Sans', 'sans-serif']
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '10rem',
+        '9xl': '15rem',
+      }
+    }
   },
   variants: {},
   plugins: []
