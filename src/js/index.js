dfb74f0d43ad25ef6a0f7b8b72bcd27573cfe974
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-20 13:36:37 +0200

    Fix eslint linter errors

--- src/js/index.js
+++ src/js/index.js
@@ -3,23 +3,23 @@ import '../css/main.css';
 
 const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 
-const getURL = (url) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
+const getURL = (url) => new Promise((resolve, reject) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = 'json';
 
-    request.onload = () => {
-      resolve(request.response);
-    };
+  request.onload = () => {
+    resolve(request.response);
+  };
 
-    request.onerror = () => {
-      reject(Error(request.statusText));
-    };
+  request.onerror = () => {
+    reject(Error('There was a network error'));
+  };
 
-    request.responseType = 'json';
-    request.send();
-  });
-};
+  request.responseType = 'json';
+  request.send();
+});
+
 
 getURL(requestURL).then((data) => {
   const header = document.querySelector('#test header');
@@ -27,33 +27,3 @@ getURL(requestURL).then((data) => {
   textElement.textContent = data.squadName;
   header.appendChild(textElement);
 });
-// catch((error) => {
-//   console.log('Error is:' + error.message);
-// });
-
-// const header = document.querySelector('#test header');
-
-// const request = new XMLHttpRequest();
-
-// request.open('GET', requestURL);
-
-// request.responseType = 'json';
-// request.send();
-
-// const populateHeader = (obj) => {
-//   const squadName = document.createElement('h1');
-//   const info = document.createElement('h2');
-//   squadName.textContent = obj.squadName;
-//   info.textContent = `Hometown: ${obj.homeTown} & formed in ${obj.formed}`;
-//   header.append(squadName);
-//   header.append(info);
-// };
-
-// request.onload = () => {
-//   const superHeroes = request.response;
-//   populateHeader(superHeroes);
-// };
-
-// window.onload = () => {
-
-// };
