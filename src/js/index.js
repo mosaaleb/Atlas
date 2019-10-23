cebe4e7a9b616c2fcf184d18e4dcc1429cfabf49
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-23 23:49:14 +0200

    Change default location

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,12 @@ import '../css/main.css';
 import APIService from './lib/apiService';
 import View from './lib/view';
 
-const weatherApiEndPoint = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0c63e66e39bfd78722433b4fbbd6c19b';
+const weatherApiEndPoint = 'http://api.openweathermap.org/data/2.5/weather?q=Cairo,eg&APPID=0c63e66e39bfd78722433b4fbbd6c19b';
 
 const loadWeatherData = (endpoint) => {
   APIService.getWeatherData(endpoint)
     .then((weatherData) => {
       View.update(weatherData);
-    })
-    .catch((error) => {
-      const errors = document.querySelector('#errors');
-      errors.textContent = error.message;
     });
 };
 
