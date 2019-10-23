74b2b3b1f1c90408894e9bad2c52e9409e1071ab
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-23 23:48:07 +0200

    Add custom fonts tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,16 @@ module.exports = {
     extend: {
       fontFamily: {
         base: ['Raleway', 'sans-serif'],
-        body: ['Work Sans', 'sans-serif']
+        body: ['Work Sans', 'sans-serif'],
+        garamond: ['Cormorant Garamond', 'serif'],
+        proza: ['Proza Libre', 'sans-serif'],
+        eczar: ['Eczar', 'serif']
       },
       fontSize: {
-        '7xl': '5rem',
-        '8xl': '10rem',
-        '9xl': '15rem',
+        '7xl' : '5rem',
+        '8xl' : '8rem',
+        '9xl' : '10rem',
+        '10xl': '12rem'
       }
     }
   },
