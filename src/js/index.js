e493667d0282c7f03985cfbf9ef0715484420447
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-24 16:22:17 +0200

    Implement search feature

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,19 @@
+// TODO: - [ ] implement search feature
+// TODO: - [ ] validate input from search
+
+
 import 'webpack-icons-installer';
 import '../css/main.css';
 import APIService from './lib/apiService';
 import View from './lib/view';
 
-const weatherApiEndPoint = 'http://api.openweathermap.org/data/2.5/weather?q=Cairo,eg&APPID=0c63e66e39bfd78722433b4fbbd6c19b';
+const AppId = '0c63e66e39bfd78722433b4fbbd6c19b';
+const defaultSearchCity = 'Cairo';
+let weatherApiEndPoint = `http://api.openweathermap.org/data/2.5/weather?q=${defaultSearchCity}&APPID=${AppId}`;
+
+const setWeatherApiEndPoint = (city) => {
+  weatherApiEndPoint = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${AppId}`;
+};
 
 const loadWeatherData = (endpoint) => {
   APIService.getWeatherData(endpoint)
@@ -13,3 +23,14 @@ const loadWeatherData = (endpoint) => {
 };
 
 loadWeatherData(weatherApiEndPoint);
+
+// UI implementation
+const searchInput = document.getElementById('search');
+searchInput.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    setWeatherApiEndPoint(searchInput.value);
+    loadWeatherData(weatherApiEndPoint);
+    searchInput.value.reset();
+  }
+});
