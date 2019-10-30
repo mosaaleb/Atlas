d4acab47a1af15c3da68da0b2b15164e0c5e868c
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-30 23:50:07 +0200

    Move event listeners and dom manipulation to UI module

--- src/js/lib/ui.js
+++ src/js/lib/ui.js
@@ -1,6 +1,10 @@
 import Controller from './controller';
+import View from './view';
+import WeatherApi from './api/weatherApi';
 
 const UI = (() => {
+  const searchInput = document.getElementById('search');
+  const closeMessage = document.getElementById('close-message');
   const checkBoxTempUnit = document.getElementById('myonoffswitch');
 
   const init = () => {
@@ -8,6 +12,15 @@ const UI = (() => {
       const isChecked = checkBoxTempUnit.checked === true;
       Controller.setSelectedTempUnit(isChecked);
       // loadWeatherData();
+    });
+    closeMessage.addEventListener('click', View.hideMessage);
+    searchInput.addEventListener('keyup', (event) => {
+      if (event.keyCode === 13) {
+        event.preventDefault();
+        View.showPreloader();
+        WeatherApi.loadWeatherData(searchInput.value);
+        searchInput.value = '';
+      }
     });
   };
 
