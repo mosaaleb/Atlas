dc0545e9bba6636e04ad496f9f75aa7ae789bde6
Muhammad Ebeid <muhammed.ebeid@gmail.com>
2019-10-30 23:46:23 +0200

    Add option to get weather city with specific city

---
+++ src/js/lib/api/weatherApi.js
@@ -0,0 +1,26 @@
+import APIService from './apiService';
+import LocationApi from './locationApi';
+import View from '../view';
+
+const WeatherApi = (() => {
+  const appId = '0c63e66e39bfd78722433b4fbbd6c19b';
+  const baseURL = 'http://api.openweathermap.org/data/2.5/weather';
+
+  const loadWeatherData = (searchedCity = null) => {
+    LocationApi.loadCityData()
+      .then((cityData) => {
+        const city = searchedCity || cityData.city;
+        const weatherApiEndPoint = `${baseURL}?q=${city}&APPID=${appId}`;
+        return APIService.getData(weatherApiEndPoint);
+      }).then((weatherData) => {
+        View.hidePreloader();
+        View.receive(weatherData);
+      }).catch(() => {
+        View.alertMessage('Not Found');
+      });
+  };
+
+  return { loadWeatherData };
+})();
+
+export default WeatherApi;
